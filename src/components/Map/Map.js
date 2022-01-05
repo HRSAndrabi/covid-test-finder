2ec08b5b751c1156c3126564c4a0f987de3e60bc
Hassan Andrabi <handrabi53@gmail.com>
2022-01-06 10:41:46 +1100

    Added barebones MapBox integration.

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,40 @@
-import React from "react";
+/* eslint import/no-webpack-loader-syntax: off */
+import React, { useRef, useEffect, useState } from "react";
 import "./Map.scss";
+import mapboxgl from "!mapbox-gl";
 
-const Map = () => {
-    return <div className="map-container"></div>;
-};
+const { REACT_APP_MAPBOX_API_TOKEN } = process.env;
+mapboxgl.accessToken = REACT_APP_MAPBOX_API_TOKEN;
+
+function Map() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+
+    useEffect(() => {
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/hassdaddy3/ckxlo17dgc4s414udisy27lev",
+            center: [133.354, -28.141],
+            zoom: 4.2,
+            maxBounds: [
+                [145.5 - 7, -40], // Southwest coordinates
+                [145.5 + 5.5, -33], // Northeast coordinates
+            ],
+            attributionControl: false,
+        });
+
+        map.current.dragRotate.disable();
+        map.current.touchZoomRotate.disableRotation();
+
+        map.current.on("load", function () {
+            map.current.resize();
+        });
+
+        // Clean up on unmount
+        return () => map.remove();
+    }, []);
+
+    return <div ref={mapContainer} className="map-container" />;
+}
 
 export default Map;
