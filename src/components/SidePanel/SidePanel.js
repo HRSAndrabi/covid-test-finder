12db09ee3c1e99ae794e8a202932495a015c9088
Hassan Andrabi <handrabi53@gmail.com>
2022-01-08 20:19:26 +1100

    Added search filter UI and side-panel hero.

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import "./SidePanel.scss";
 import { MdSearch } from "react-icons/md";
 import _uniqueId from "lodash/uniqueId";
+import { MdLink } from "react-icons/md";
+import { Chip } from "@mui/material";
 
 const SidePanel = (props) => {
     const [drawerOpen, setDrawerOpen] = useState(props.drawerOpen);
-    const [id, setID] = useState(_uniqueId("form-"));
+    const [filter, setFilter] = useState({
+        open: false,
+        "walk-in": false,
+        "drive-through": false,
+        "all-ages": false,
+    });
 
     const drawerOpenHandler = () => {
         props.drawerOpenHandler(true);
@@ -15,15 +22,82 @@ const SidePanel = (props) => {
         setDrawerOpen(false);
     };
 
+    const filterChangeHandler = (event) => {
+        setFilter({
+            ...filter,
+            [event.target.innerText]: !filter[event.target.innerText],
+        });
+    };
+
     useEffect(() => {
         setDrawerOpen(props.drawerOpen);
         return () => {};
     }, [props]);
 
+    const inputFieldId = Math.floor(Math.random() * 10000);
     return (
         <>
             <div className="desktop-drawer">
-                <div className="drawer-inner">{props.renderedFeatures}</div>
+                <div className="drawer-inner">
+                    <div className="drawer-hero">
+                        <div className="drawer-hero__header">
+                            COVID-19 test finder
+                        </div>
+                        <div className="drawer-hero__links">
+                            <MdLink /> covidtestnearme.app
+                        </div>
+                    </div>
+                    <div className="drawer-search">
+                        <div className="search">
+                            <MdSearch className="search__icon" />
+                            <input
+                                id="search__input"
+                                className="search__input"
+                                type="text"
+                                name={inputFieldId}
+                                placeholder="Search testing sites"
+                            />
+                        </div>
+                        <div className="search-filters">
+                            <div className="search-filters__label">Filter:</div>
+                            <Chip
+                                label="open"
+                                color={filter.open ? "primary" : "default"}
+                                size="small"
+                                onClick={filterChangeHandler}
+                            />
+                            <Chip
+                                label="walk-in"
+                                color={
+                                    filter["walk-in"] ? "primary" : "default"
+                                }
+                                size="small"
+                                onClick={filterChangeHandler}
+                            />
+                            <Chip
+                                label="drive-through"
+                                color={
+                                    filter["drive-through"]
+                                        ? "primary"
+                                        : "default"
+                                }
+                                size="small"
+                                onClick={filterChangeHandler}
+                            />
+                            <Chip
+                                label="all ages"
+                                color={
+                                    filter["all-ages"] ? "primary" : "default"
+                                }
+                                size="small"
+                                onClick={filterChangeHandler}
+                            />
+                        </div>
+                    </div>
+                    <div className="testing-site-list">
+                        {props.renderedFeatures}
+                    </div>
+                </div>
             </div>
             <div
                 className="mobile-drawer"
@@ -39,7 +113,8 @@ const SidePanel = (props) => {
                             id="search__input"
                             className="search__input"
                             type="text"
-                            name={id}
+                            name={inputFieldId}
+                            placeholder="Search testing sites"
                         />
                     </div>
                 </div>
